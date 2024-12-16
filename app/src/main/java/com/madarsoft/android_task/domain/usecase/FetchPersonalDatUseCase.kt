aaa51334963d327a5cd9e1955c20da933c35ef12
mohamedsafwatnassar <mohamed.nasar8710@gmail.com>
2024-12-16 03:57:25 +0200

    implementation of get personal data from local database

--- app/src/main/java/com/madarsoft/android_task/domain/usecase/FetchPersonalDatUseCase.kt
+++ app/src/main/java/com/madarsoft/android_task/domain/usecase/FetchPersonalDatUseCase.kt
@@ -1,13 +1,16 @@
 package com.madarsoft.android_task.domain.usecase
 
+import com.madarsoft.android_task.domain.model.PersonalData
 import com.madarsoft.android_task.domain.repository.LocalPersonalDataRepository
+import com.madarsoft.android_task.util.DataState
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class FetchPersonalDatUseCase @Inject constructor(
     private val localRepository: LocalPersonalDataRepository,
 ) {
 
-    suspend fun fetchPersonalDataByIdFromLocal(personalDataId: Int)  {
-        localRepository.fetchPersonalDataByIdFromLocal(personalDataId)
+    suspend fun fetchPersonalDataByIdFromLocal(personalDataId: Int) : Flow<DataState<PersonalData>> {
+        return localRepository.fetchPersonalDataByIdFromLocal(personalDataId)
     }
 }
